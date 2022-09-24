9f607015a0d3b0947fffb3adf7772892d23a8cf6
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-09-23 20:45:56 -0500

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,14 +24,33 @@ export class AppComponent {
 
   //ngFor
 
-  muestraEmpleados:boolean=false;
+  muestraEmpleados:boolean=true;
 
   mostrarEmpleados(){
     this.muestraEmpleados =!this.muestraEmpleados;
   }
 
-  muestraEmpleadosCalifiMayor7(){
-    
+  constructor(){
+    this.muestraEmpleadosCalificacionMayor7();
+    this.mueestraEmpleadosMaximaCalificacion();
+  }
+
+  empCalificaM7:any[]=[];
+  empCalifMaxima:any[]=[];
+
+  muestraEmpleadosCalificacionMayor7(){
+    let empCalif7 = this.empleados.filter((empleado) => empleado.calificacion>7);
+    this.empCalificaM7 = empCalif7;
+    console.table(this.empCalificaM7);
+  }
+  
+  mueestraEmpleadosMaximaCalificacion(){
+    let maximaCalificacion = this.empCalificaM7.reduce((x,y) => {
+      return (x.calificacion<=y.calificacion?y:x)
+    })
+
+    this.empCalifMaxima=this.empCalificaM7.filter((empleado) => empleado.calificacion === maximaCalificacion.calificacion);
+    console.log(this.empCalifMaxima);
   }
 
   empleados:any[]=[
